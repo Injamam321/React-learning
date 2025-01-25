d2aef813f8568ede5de29289b97fecb79c47799a
Md. Injamam Hossain <injamam075@gmail.com>
2025-01-25 11:50:40 +0600

    first react commit

---
+++ src/Blogs.jsx
@@ -0,0 +1,10 @@
+function Blogs(){
+    return(
+        <div>
+
+            <h2>I am imjamam Hossain new react developer</h2>
+        </div>
+    )
+}
+
+export default Blogs;
---
+++ src/Product.jsx
@@ -0,0 +1,11 @@
+const products = () =>{
+
+    return(
+        <div>
+
+            <p>I am a Good product supplier </p>    
+        </div>
+    )
+}
+
+export default products;
